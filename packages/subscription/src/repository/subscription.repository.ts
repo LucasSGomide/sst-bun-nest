cc566315ef4f83423ea794da1b39bb522373b921
Lucas Gomide <lucas.sgomide92@gmail.com>
2025-07-07 21:37:09 -0300

    feat: implement find all method in subscription repository

--- packages/subscription/src/repository/subscription.repository.ts
+++ packages/subscription/src/repository/subscription.repository.ts
@@ -1,9 +1,13 @@
+import { inject, injectable } from 'tsyringe';
 import type { ISubscriptionRepository } from '../domain/interfaces/subscription-repository.interface';
+import type { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import { schema } from '../infra/database/connection';
 
+@injectable()
 export class SubscriptionRepository implements ISubscriptionRepository {
-    constructor(private dbClient: any) {}
+    constructor(@inject('DBClient') private dbClient: NeonHttpDatabase<typeof schema>) {}
 
-    async findAll() {
-        return [];
+    async findAll(): Promise<Array<any>> {
+        return this.dbClient.select().from(schema.subscriptionsSchema);
     }
 }
