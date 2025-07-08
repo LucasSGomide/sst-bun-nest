01c1b6150d85ca943998a991d91e0e157f56bec9
Lucas Gomide <lucas.sgomide92@gmail.com>
2025-07-07 21:36:37 -0300

    refactor: add type to database connection

--- packages/subscription/src/infra/database/connection.ts
+++ packages/subscription/src/infra/database/connection.ts
@@ -1,17 +1,19 @@
 import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
 import { subscriptionsSchema } from './schemas/subscriptions';
 
 // Bun automatically loads the DATABASE_URL from .env.local
 // Refer to: https://bun.sh/docs/runtime/env for more information
 
-export function databaseConnection() {
+export const schema = { subscriptionsSchema };
+
+export function databaseConnection(): NeonHttpDatabase<typeof schema> {
     try {
         console.log('Init neon database connection...');
         const sql = neon(process.env.DATABASE_URL!);
         console.log('Neon database connected...');
 
-        return drizzle(sql, { casing: 'snake_case', schema: { ...subscriptionsSchema } });
+        return drizzle(sql, { casing: 'snake_case', schema });
     } catch (error) {
         console.error('Error initialzing neon database: ', error);
         throw error;
