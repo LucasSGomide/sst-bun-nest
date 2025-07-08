2336506148fa6d4c7d461e6cfb863845b229bb9e
Lucas Gomide <lucas.sgomide92@gmail.com>
2025-07-07 21:40:10 -0300

    chore: config esbuild plugin on lambda build and update handler path

--- infra/api.ts
+++ infra/api.ts
@@ -1,14 +1,32 @@
-import { databaseUrl } from "./secrets";
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
+import { databaseUrl } from './secrets';
 
-export const api = new sst.aws.ApiGatewayV2("ApiGateway", {
-  accessLog: {
-    retention: "1 day",
-  },
+export const api = new sst.aws.ApiGatewayV2('ApiGateway', {
+    accessLog: {
+        retention: '1 day',
+    },
 });
 
-api.route("GET /api", {
-  handler: "packages/subscription/src/infra/lambda.helloWorldHandler",
-  environment: {
-    DATABASE_URL: databaseUrl.value
-  }
+api.route('ANY /subscriptions', {
+    handler: 'packages/subscription/src/infra/api.subscriptionsHandler',
+    environment: {
+        DATABASE_URL: databaseUrl.value,
+    },
+    nodejs: {
+        esbuild: {
+            plugins: [esbuildPluginTsc({ tsconfigPath: '../tsconfig.json' })],
+        },
+    },
+});
+
+api.route('ANY /subscriptions/{proxy+}', {
+    handler: 'packages/subscription/src/infra/api.subscriptionsHandler',
+    environment: {
+        DATABASE_URL: databaseUrl.value,
+    },
+    nodejs: {
+        esbuild: {
+            plugins: [esbuildPluginTsc({ tsconfigPath: '../tsconfig.json' })],
+        },
+    },
 });
